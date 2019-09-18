eeb709f2bf76ef472e26a1781858faba328caea0
jay150503 <jaypro27@gmail.com>
2019-09-18 12:51:23 +0700

    penambahan file

--- outarray.php
+++ outarray.php
@@ -4,5 +4,8 @@ if(isset($_SESSION['login'])){
 unset ($_SESSION);
 session_destroy();
     header("location:logarray.php");
+}else {
+    die("username atau password anda salah 
+    silahkan kembali login <a href=logarray.php> Login </a>");
 }
 ?>
